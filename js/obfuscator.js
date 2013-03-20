8066621ad6132184401ce6b58ebb8596e6ad6516
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2013-03-20 20:21:16 +0100

    added random name generation

--- js/obfuscator.js
+++ js/obfuscator.js
@@ -1,7 +1,41 @@
+var getRandName = function() {
+	var pool = ["0", "O"];
+	var name = "";
+
+	var length = Math.floor(Math.random() * 20) + 5;
+	for (var i = 0 ; i < length ; i++) {
+		name += pool[Math.floor(Math.random() * pool.length)];
+	}
+
+	return name;
+}
+
+
+var getKeys = function(myDict) {
+	res = [];
+	for(i in myDict) {
+		res.push(myDict[i][0]);
+	}
+}
+
+var populateDictionary = function(toSubst, dict) {
+	for(i in toSubst) {
+		do {
+			var name = getRandName();
+		} while(jQuery.inArray(name, getKeys(dict)) >= 0)
+		dict.push(name);
+	}
+	return dict;
+}
+
+
 var obfuscate = function() {
 	var raw = $("#raw").val();
 
+	substitutionDict = [];
+
 	raw = raw.toUpperCase();
+	raw += getRandName();
 
 	$("#obfuscated").val(raw);
 }
