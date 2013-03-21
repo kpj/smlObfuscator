04f90e4687bcaec3801a5b8c9f6de503f50810ba
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2013-03-21 14:47:08 +0100

    improved documentation

--- js/obfuscator.js
+++ js/obfuscator.js
@@ -1,9 +1,12 @@
-var getRandName = function() {
+/*
+	This function creates a random string consisting out of "O" and "0".
+	Input:	length	- length of output string (int)
+	Output:	name		- random string (string)
+*/
+var getRandName = function(length) {
 	var pool = ["0", "O"];
 	var name = "";
 
-	//var length = Math.floor(Math.random() * 20) + 5;
-	var length = 10;
 	for (var i = 0 ; i < length ; i++) {
 		name += pool[Math.floor(Math.random() * pool.length)];
 	}
@@ -11,25 +14,39 @@ var getRandName = function() {
 	return name;
 }
 
-
+/*
+	This function returns the keys of a list of lists which represents a dictionary in the following form: [["key", "value"], ..]
+	Input:	myDict	- dictionary from which we want to get the keys
+	Output:	res			-	list of keys
+*/
 var getKeys = function(myDict) {
 	res = [];
 	for(i in myDict) {
 		res.push(myDict[i][0]);
 	}
+	return res;
 }
 
+/*
+	This function adds the given substitution variables to the given dictionary
+	Input:	toSubst	-	list a strings that should be replaced later on
+	Output:	dict		- already existing, older dictionary
+*/
 var populateDictionary = function(toSubst, dict) {
 	for(i in toSubst) {
 		do {
-			var name = getRandName();
+			var name = getRandName(10);
 		} while(jQuery.inArray(name, getKeys(dict)) >= 0)
 		dict.push(name);
 	}
 	return dict;
 }
 
-
+/*
+	This function is the wrapper for the obfuscation
+	Input:	-
+	Output:	-
+*/
 var obfuscate = function() {
 	var raw = $("#raw").val();
 
