95f1362982c9b34540384d922983f7f45f659488
Tom Wiesing <tkw01536@gmail.com>
2013-03-26 00:24:43 +0100

    It works!

--- js/obfuscator.js
+++ js/obfuscator.js
@@ -1,11 +1,251 @@
 /*
+	A bunch of keywords in SML. 
+	These should not be changed. 
+	
+	May be incomplete. 
+*/
+var SMLKeyWords = 
+[
+	/* Functions, datatypes, etc */
+	"fun",
+	"fn",
+	"datatype",
+	"functor",
+	"infix",
+	"infixr",
+	"nonfix",
+	"open",
+
+
+	"LESS",
+	"EQUAL",
+	"GREATER",
+	
+
+	/* branching & scoping */
+	
+	"local",
+	"in",
+	"end",
+
+	"let",
+	"if",
+	"then",
+	"else",
+
+	"case",
+
+	"andalso",
+	"orelse",
+	
+
+	/* Variables */
+	"val",
+	"type",
+	"structure",
+	"struct",
+	"STRUCT",
+	
+	"_",
+	"signature",
+	"SIG",
+	"sig",
+	
+	/* types */
+	"bool",
+	"true",
+	"false", 
+
+	"int",
+
+	"real",
+
+	"list",
+	"nil",
+
+	"string",
+	
+	"char",
+
+	"word",
+	
+	"ref",
+
+	"exception",
+	"raise",
+	"handle",
+
+	"option",
+	"SOME",
+	"NONE",
+
+	"unit",
+
+	"array",
+	"vector",
+
+	/* misc */
+	"op",
+	"of",
+	"rec",
+
+	/* builtin functions */
+	"div",
+	"mod",
+	
+	"length",
+	"map",
+	"foldl", 
+	"foldr",
+	
+	"implode", 
+	"explode",
+	
+	"print",
+	"substring",
+	"revserse",
+	"exn"
+];
+
+/*
+	@param code	A bunch of SML Code
+
+	turns code from SML into the following form: 
+	
+	[
+		CODE
+		STUFF_IN_QUTATION_MARKS_WE_SHOULD_NOT_TOUCH
+		CODE
+		STUFF_IN_QUTATION_MARKS_WE_SHOULD_NOT_TOUCH
+		...
+	]
+
+	Also removes comments from the code.  
+	
+*/
+var parseCode = function(code){
+	var bracket_open = 0;
+	var quotation_open = false;
+
+	var res = [""];
+	
+	for(var i=0;i<code.length;i++){
+		if(quotation_open){
+			if(code[i] == "\\" && code[i+1] == "\""){ // \" are ignored
+				res[res.length-1] += "\\\"";
+				i++;
+			} else if(code[i] == "\""){ //closing quoattion mark
+				res[res.length-1] += "\"";
+				res.push("");
+				quotation_open = false;
+			} else { //nothign special
+				res[res.length-1] += code[i];
+			}
+		} else {
+			if(code[i] == "\"" && bracket_open == 0){ //openinf a quotation amrk while not in comment
+				res.push("\"");
+				quotation_open = true;
+			} else if(code[i] == "(" && code[i+1] == "*"){ //open comment
+				bracket_open++;	
+				i++;	
+			} else if (code[i] == "*" && code[i+1] == ")" && bracket_open > 0){ //close bracket
+				bracket_open--;	
+				i++;	
+			} else if (bracket_open == 0){ //add some stuff
+				res[res.length-1] += code[i];	
+			}
+		}
+			
+	}
+	return res;
+}
+
+/*
+	Takes parseCode output and turns it back into a code string. 
+	@param parsedCode some parsed code. 
+*/
+var buildCode = function(parsedCode){
+	return parsedCode.join(""); 
+};
+
+/*
+	Gets all the key words from the SML code. 
+	@param  parsedCode some parsed code. 
+	@exceptions an array of keywords to ignore. 
+*/
+var getWords = function(parsedCode, exceptions){
+	var res = [];
+	for(var i=0;i<parsedCode.length;i=i+2){
+		
+		var tmp = parsedCode[i].match(/[a-zA-Z0-9_]+/g);
+		if(tmp == null || typeof tmp == "undefined"){
+			tmp = [];		
+		}
+		for(var j=0;j<tmp.length;j++){
+			var matched = tmp[j];
+			if(!(parseInt(matched).toString() == matched) && res.indexOf(matched) == -1 && exceptions.indexOf(matched) == -1 && SMLKeyWords.indexOf(matched) == -1){/* IE <= 8 not supported! */
+				/* no integer && we have not already matched we are also no keywords*/
+				res.push(matched);
+			}		
+		}	
+	}
+	return res;
+}
+
+/*
+	Parses structures [Structure].[Object] used in the code and ignores them. 
+	@param parsedCode parsed code. 
+	
+	Returns new parsed code object. 
+*/
+var parseStructures = function(parsedCode){
+	for(var i=0;i<parsedCode.length;i=i+2){
+		var tmp = parsedCode[i].match(/[a-zA-Z0-9_]+[.][a-zA-Z0-9_]+/g);
+		if(tmp == null || typeof tmp == "undefined"){
+			tmp = [];		
+		}
+		for(var j=0;j<tmp.length;j++){
+			parsedCode = IgnoreWords(parsedCode, tmp[j]);
+			
+		}	
+	}
+
+	return parsedCode;
+}
+
+
+/*
+	Replaces all the keywords with all the new keywords
+	@param parsedCode A bunch of parsed code. 
+	@param dic A dictionary containing keyWord => replacement pairs. 
+*/
+var replaceWords = function(parsedCode, dic){
+	for(var i=0;i<parsedCode.length;i=i+2){
+		parsedCode[i] = parsedCode[i].replace(/[a-zA-Z0-9_]+/g, function(old){
+			if(dic.hasOwnProperty(old)){
+				return dic[old];
+			} else {
+				return old;
+
+			}	
+		});
+			
+	}
+	return parsedCode;
+}
+
+/*
 	This function creates a random string consisting out of "O" and "0".
 	Input:	length	- length of output string (int)
-	Output:	name		- random string (string)
+		forbidden - array of string that is forbidden
+	Output:	name	- random string (string)
 */
-var getRandName = function(length) {
+var getRandName = function(length, forbidden) {
+
 	var pool = ["0", "O"];
-	var name = "";
+	var name = "O";
+
+	length--;
 
 	for (var i = 0 ; i < length ; i++) {
 		name += pool[Math.floor(Math.random() * pool.length)];
@@ -14,32 +254,68 @@ var getRandName = function(length) {
 	return name;
 }
 
-/*
-	This function returns the keys of a list of lists which represents a dictionary in the following form: [["key", "value"], ..]
-	Input:	myDict	- dictionary from which we want to get the keys
-	Output:	res			-	list of keys
-*/
-var getKeys = function(myDict) {
-	res = [];
-	for(i in myDict) {
-		res.push(myDict[i][0]);
-	}
-	return res;
+var randomNumber = function(a, b){
+	return a+Math.floor(Math.random() * (b-a));
 }
 
 /*
-	This function adds the given substitution variables to the given dictionary
-	Input:	toSubst	-	list a strings that should be replaced later on
-	Output:	dict		- already existing, older dictionary
+	forces to ignore text in parsedCode. 
+	@param parsedCode The parsed code. 
+	@param text The text to ignore.  
 */
-var populateDictionary = function(toSubst, dict) {
-	for(i in toSubst) {
-		do {
-			var name = getRandName(10);
-		} while(jQuery.inArray(name, getKeys(dict)) >= 0)
-		dict.push(name);
+var IgnoreWords = function(parsedCode, text){
+	var res = [];
+	var state = false;
+	for(var i=0;i<parsedCode.length;i++){
+		if(state == false){
+			var chunk = parsedCode[i];
+			var chunks = chunk.split(text);
+			for(var j=0;j<chunks.length-1;j++){
+				res.push(chunks[j]);
+				res.push(text);			
+			}
+			res.push(chunks[chunks.length-1]);
+			state = true;
+		} else {
+			res.push(parsedCode[i]);
+			state = false;		
+		}	
 	}
-	return dict;
+
+	return res;
+}
+
+/* generate random stuff that does nothing. */
+var generateRubbish = function(){
+	if(Math.random() < 0.8){
+		//put in a random quote
+		return randomComments[Math.floor(Math.random() * randomComments.length)];
+	} else {
+		var j = Math.floor(Math.random() * 20);
+		var stuff = function(){
+			return getRandName(randomNumber(j, 42), []);
+		}
+
+		var args = function(){
+			var stack = [];
+			for(var i=0;i<j;i++){
+				stack.push(stuff());
+			}	
+			return stack.join(", ");
+		}
+
+		var myname = stuff();
+		var res = "\nfun "+myname+"("+args()+") = nil\n";
+		for(var i=0;i<j;i++){
+			if(Math.random() < 0.2){
+				res += "| "+myname+"("+args()+") = "+stuff()+"("+args()+")\n";
+			} else if(Math.random() < 0.2){
+				res += "| "+myname+"("+args()+") = raise "+stuff()+"("+args()+")\n";
+			}
+			
+		}
+		return res;
+	}
 }
 
 /*
@@ -52,7 +328,41 @@ var obfuscate = function() {
 
 	substitutionDict = [];
 
-	raw = raw.toUpperCase();
+	var parsedCode = parseStructures(parseCode(raw)); //parse code and structures
 
-	$("#obfuscated").val(raw);
+	
+	var exceptions = $("#ignore_funcs").val().split(" ").join("").split(","); //get the exceptions. 
+
+	var words = getWords(parsedCode, exceptions); //get words. 
+	var replacements = [];
+	var newWords = {};
+
+
+	//generate random new ones
+	for(var i=0; i<words.length;i++){
+		var key = words[i];
+		var repl = getRandName(randomNumber(10, 20)); //random length
+		while(replacements.indexOf(repl) != -1){
+			repl = getRandName(randomNumber(10, 20));
+		}
+		replacements.push(repl);
+		newWords[key] = repl;
+	}
+
+	var replacedCode = buildCode(replaceWords(parsedCode, newWords))+";";
+	
+	if($("#add_garbage").is(":checked")){
+		replacedCode = replacedCode.replace(/[\n]/g, function(m){
+			if(Math.random() < 0.2){
+				return "(* "+generateRubbish()+" *)\n";
+			} else {
+				return "\n";		
+			}
+		
+		});
+	}
+
+	
+
+	$("#obfuscated").val(replacedCode); // put it back. 
 }
---
+++ js/random.js
@@ -0,0 +1,63 @@
+/* a bunch of random comments. Feel free to add more. */
+var randomComments = [
+
+
+	/* a bunch of random stuff I just had in my mind. */
+	"2 am in the IRC Quiet Study Area",
+	"The GIGO Principle: Garbage In, Garbage Out ",
+	"Applets, Not Craplets",
+	"This is a nice comment. ",
+	"Do not delete this comment. ",
+	"I forgot to delete this comment. ",
+	"This is an epic comment. ",
+	"Nothing here. ",
+	"I'm just a random comment. ",
+	"I support "+navigator.userAgent,
+	"I got to stare at the slides more. ",
+	"It works!",
+	"Causes a kernel panic. ",
+	"Follow the train, CJ!",
+	"Turing complete. ",
+	"42. ",
+	"Ihr seid doch alle wahnsinnig! ",
+	"Removed herobrine. ",
+	"from The Book of Mozilla. ",
+	"smaller on the outside. ",
+	"cookies are delicious delicacies. ",
+
+
+	/* jokes */
+
+	"Mathematician: Pi is the ratio of the circumference of a circle to its diameter. \n"+
+	"Engineer: Pi is about 22/7. \n"+
+	"Physicist: Pi is 3.14159 plus or minus 0.000005 \n"+
+	"Computer Programmer: Pi is 3.141592653589 in double precision. \n"+
+	"Nutritionist: You one track math-minded fellows, Pie is a healthy and delicious dessert!",
+	
+	"\"A mathematician is a device for turning coffee into theorems\" (P. Erdos) \n"+
+	"Addendum: American coffee is good for lemmas. ",
+	
+	"A mathematician believes nothing until it is proven, \n"+
+	"a physicist believes everything until it is proven wrong, \n"+
+	"a chemist doesn't care and \n"+
+	"a biologist doesn't understand the question. ",
+
+	"This is a one line proof...if we start sufficiently far to the left.",
+	
+	"The problems for the exam will be similar to the discussed in the class. Of course, the numbers will be different. But not all of them. Pi will still be 3.14159... ",
+	
+	"Q: Why do Computer Scientists get Halloween and Christmas mixed up? \n"+
+	"A: Because Oct. 31 = Dec. 25",
+	
+	"Math problems? Call 1-800-[(10x)(13i)2]-[sin(xy)/2.362x]. ",
+
+	'A man flying in a hot air balloon suddenly realizes he\'s lost. He reduces height and spots a man down below. He lowers the balloon further and shouts to get directions, "Excuse me, can you tell me where I am?" \n'+
+	'The man below says: "Yes. You\'re in a hot air balloon, hovering 30 feet above this field." \n'+
+	'"You must work in Information Technology," says the balloonist. \n'+
+	'"I do" replies the man. "How did you know?" \n'+
+	'"Well," says the balloonist, "everything you have told me is technically correct, but It\'s of no use to anyone." \n'+
+	'The man below replies, "You must work in management." \n'+
+	'"I do," replies the balloonist, "But how\'d you know?" \n'+
+	'"Well", says the man, "you don’t know where you are or where you’re going, but you expect me to be able to help. You’re in the same position you were before we met, but now it’s my fault." \n'
+
+];
